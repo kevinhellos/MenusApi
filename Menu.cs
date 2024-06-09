e4784dcba09b30aafc0a647517797cf37df104f1
Kevin H <126497052+kevinhellos@users.noreply.github.com>
2024-06-09 19:54:41 +0800

    Create Menu.cs

---
+++ Menu.cs
@@ -0,0 +1,9 @@
+namespace MenusApi;
+
+public class Menu
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public string? Category { get; set; }
+}
